2ae9f7fe37cf90c5833f30c26da547a89fb81d86
Abe Rudy <aberudy@gmail.com>
2017-10-24 09:38:13 -0400

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,25 @@
-# Enter your procedural solution here!
+def collect_multiples(max)
+  max = max
+  i = 1
+  solution = []
+  while i < max
+    if i % 5 == 0 || i % 3 == 0
+      solution << i
+    end
+    i+=1
+  end
+  solution
+end
+
+def sum_multiples(max)
+  max = max
+  i = 1
+  solution = 0
+  while i < max
+    if i % 5 == 0 || i % 3 == 0
+      solution += i
+    end
+    i+=1
+  end
+  solution
+end
--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,33 @@
-# Enter your object-oriented solution here!
+require 'pry'
+
+class Multiples
+  attr_accessor :min, :max, :total
+
+  def initialize(max, min=1)
+    @min = min
+    @total = 0
+    @max = max
+  end
+
+  def sum_multiples
+    (@min...@max).each do |num|
+      if num % 3 == 0 || num % 5 == 0
+        @total += num
+      end
+    end
+    @total
+  end
+
+  def collect_multiples
+    collection = []
+    (@min...@max).select do |num|
+      if num % 3 == 0 || num % 5 == 0
+        collection << num
+      end
+    end
+    collection
+  end
+
+
+end
+#Pry.start
